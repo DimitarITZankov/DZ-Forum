971aec45227b737d098ae103940cc1ec614ecf67
Dimitar <dimitarzankovit@gmail.com>
2025-11-27 12:39:50 +0200

    added wait_for_db file

--- app/forumapp/models.py
+++ app/forumapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser,PermissionsMixin,BaseUserManager,)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+	#Manager for users
+	def create_user(self,email,password=None,**extra_fields): #The **extra_fields here are provoding all the non-must fields to our model, 
+															 #so whenever we add a new field to our model, we dont need to pass it
+		if not email:
+			raise ValueError('User must have an email address')
+		user = self.model(email=self.normalize_email(email),**extra_fields) #we normalize the email directly when we create the user instead of creating a variable and then assing it
+		user.set_password(password)
+		user.save(using=self._db) #This supports adding multiple databases to one project
+		return user
+
+	def create_superuser(self,email,password):
+		#Creating superuser using the django CLI
+		user = self.create_user(email,password)
+		user.is_staff = True
+		user.is_superuser = True
+		user.save(using=self._db)
+		return user
+
+class User(AbstractBaseUser,PermissionsMixin):
+	email = models.EmailField(max_length=255,unique=True)
+	name = models.CharField(max_length=255)
+	is_active = models.BooleanField(default=True)
+	is_staff = models.BooleanField(default=False)
+
+	objects = UserManager() #We assign our model to the custom user manager
+
+	USERNAME_FIELD = 'email'
